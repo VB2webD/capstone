df52662ded38de05a756a6b9753f88bfd698d727
Volker Bransky <vBranskygooglemail.com>
2021-11-02 12:28:04 +0100

    Setup commit / add json-server &  babelrc, fix bug test conflicts, create Shop.json

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,11 @@ module.exports = {
     "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$":
       "<rootDir>/__mocks__/fileMock.js",
   },
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/.next/",
+    "<rootDir>/cypress/",
+  ],
   testEnvironment: "jsdom",
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
@@ -29,6 +33,5 @@ module.exports = {
   transformIgnorePatterns: [
     "/node_modules/",
     "^.+\\.module\\.(css|sass|scss)$",
-    "/cypress",
   ],
 };
