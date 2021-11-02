3b148d9cffcf64d79a718b2e4a54a78c24ac8062
Volker Bransky <vBranskygooglemail.com>
2021-11-02 13:23:42 +0100

    create basic header / embed in index for testing

---
+++ components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Header = ({ title }) => {
+  return (
+    <StyledHeader>
+      <span>{title}</span>
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.header`
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  height: 4rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--bg-color);
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-
 import Footer from "../components/Footer";
+import { Header } from "../components/Header";
 
 export default function Home() {
   return (
     <>
+      <Header title={"index"} />
       <h1>Hello World</h1>
       <Footer />
     </>
