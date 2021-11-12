a7769dada7d963054e00223b0fa0caf1f399cb44
Volker Bransky <vBranskygooglemail.com>
2021-11-12 17:46:42 +0100

    fix getBags util, + comment for [route] later

--- utils/data/getBags.js
+++ utils/data/getBags.js
@@ -1,9 +1,19 @@
 import client from "../database";
 
-export default async function getBags() {
+export async function getBags() {
   await client.connect();
   const db = client.db("capstone_candyshop");
   const collection = db.collection("bags");
   const data = await collection.find().toArray();
   return data;
 }
+
+//dynamic:
+
+// export async function getBag(slug) {
+//   await client.connect();
+//   const db = client.db(capstone_candyshop);
+//   const collection = db.collection("bags");
+//   const data = await collection.findOne({ slug });
+//   return data;
+// }
