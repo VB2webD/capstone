c9fa12627d7b84bac6084417bfe280cf149062af
Volker Bransky <vBranskygooglemail.com>
2021-11-16 17:48:35 +0100

    move main Layout to layouts Folder

--- components/layouts/layout/Header.jsx
+++ components/layouts/layout/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import ShoppingCart from "../ShoppingCart";
+import ShoppingCart from "../../ShoppingCart";
 
 const Header = ({ title, hasCart }) => {
   return (
