64da02d55a4224d6e440901686f7abb65cd9669f
Volker Bransky <vBranskygooglemail.com>
2021-11-18 18:08:21 +0100

    cleanup header

--- components/layouts/layout/Head.jsx
+++ components/layouts/layout/Head.jsx
@@ -8,12 +8,6 @@ const HtmlHead = ({ title }) => {
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="icon" href="/candyHeader.ico"></link>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
-        rel="stylesheet"
-      />
     </Head>
   );
 };
