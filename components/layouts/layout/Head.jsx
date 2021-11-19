3f7246fbb701d3843ce9bed36229f28d60e0dcf7
Volker Bransky <vBranskygooglemail.com>
2021-11-19 17:01:10 +0100

    fix  msg on vercel crossorigin => crossOrigin

--- components/layouts/layout/Head.jsx
+++ components/layouts/layout/Head.jsx
@@ -9,7 +9,7 @@ const HtmlHead = ({ title }) => {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="icon" href="/candyHeader.ico"></link>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link
         href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Source+Code+Pro:ital,wght@0,400;0,500;1,500&display=swap"
         rel="stylesheet"
