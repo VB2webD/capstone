71cfa771fc61c72dd348e884ca5d1a5d4631e3a2
Volker Bransky <vBranskygooglemail.com>
2021-11-22 16:24:21 +0100

    remove console.logs from localStorage

--- utils/useLocalStorage.js
+++ utils/useLocalStorage.js
@@ -1,9 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 
 export const useLocalStorageState = (key, defaultValue) => {
-  const [value, setValue] = useState(
-    () => (console.log("[useLocalStorage] init"), defaultValue)
-  );
+  const [value, setValue] = useState();
 
   useEffect(() => {
     if (
@@ -14,10 +12,8 @@ export const useLocalStorageState = (key, defaultValue) => {
       try {
         const item = window.localStorage.getItem(key);
         if (item) {
-          console.log("[useLocalStorage] getItem Effect / found", item);
           setValue(JSON.parse(item));
         } else {
-          console.log("[useLocalStorage] getItem Effect / just init ls");
           window.localStorage.setItem(key, JSON.stringify(defaultValue));
         }
       } catch (error) {
@@ -38,13 +34,8 @@ export const useLocalStorageState = (key, defaultValue) => {
     ) {
       try {
         if (!isInitialRender.current) {
-          console.log("[useLocalStorage] setItem Effect / new value is", value);
           window.localStorage.setItem(key, JSON.stringify(value));
         } else {
-          console.log(
-            "[useLocalStorage] setItem Effect / SKIPPED / value would have been",
-            value
-          );
           isInitialRender.current = false;
         }
       } catch (error) {
