26264f46ed2c771cdf420c6dcdbae43349524231
Volker Bransky <vBranskygooglemail.com>
2021-11-22 20:27:51 +0100

    use old version of LS due to bug in rework

--- utils/useLocalStorage.js
+++ utils/useLocalStorage.js
@@ -1,4 +1,43 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
+
+export const useLocalStorageState = (key, defaultValue) => {
+  const [value, setValue] = useState(defaultValue);
+
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      !!navigator.cookieEnabled &&
+      !!window.localStorage
+    ) {
+      try {
+        const item = window.localStorage.getItem(key);
+        setValue(item ? JSON.parse(item) : defaultValue);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      return defaultValue;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      !!navigator.cookieEnabled &&
+      window.localStorage
+    ) {
+      try {
+        window.localStorage.setItem(key, JSON.stringify(value));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+/* import { useEffect, useRef, useState } from "react";
 
 export const useLocalStorageState = (key, defaultValue) => {
   const [value, setValue] = useState();
@@ -46,3 +85,4 @@ export const useLocalStorageState = (key, defaultValue) => {
 
   return [value, setValue];
 };
+*/
